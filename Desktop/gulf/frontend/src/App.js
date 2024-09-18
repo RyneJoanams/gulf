7f6340c9047179ac66c6edd96507ec2534b24f1a
Ryne <rynejoanamslukhwili@gmail.com>
2024-09-18 20:15:41 +0300

    deleted unnecessary component
    removed department selection component.

--- Desktop/gulf/frontend/src/App.js
+++ Desktop/gulf/frontend/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "../src/pages/Home/Home.jsx";
 import Admin from './pages/Admin';
 import Login from './components/Login';
-import DepartmentSelection from './components/DepartmentSelection';
 
 const App = () => {
   return (
@@ -11,10 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Login" element={<Login />} /> 
-        <Route path="/DepartmentSelection" element={<DepartmentSelection />} />
         <Route path="/Admin" element={<Admin />} />
-
-        {/* If DepartmentSelection is not used as a page, remove its route */}
         {/* Add more routes as needed */}
       </Routes>
     </Router>
